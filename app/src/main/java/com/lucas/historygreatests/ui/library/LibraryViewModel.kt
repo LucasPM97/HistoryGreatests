42fa59d877123970a773d4a51bc1492708ac539c
Lucas <lucasmontanari.97@gmail.com>
2020-09-26 19:14:35 -0300

    Refactor base project names

--- app/src/main/java/com/lucas/historygreatests/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/lucas/historygreatests/ui/library/LibraryViewModel.kt
@@ -1,13 +1,13 @@
-package com.lucas.historygreatests.ui.dashboard
+package com.lucas.historygreatests.ui.library
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class LibraryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/lucas/historygreatests/ui/trending/TrendingFragment.kt
+++ app/src/main/java/com/lucas/historygreatests/ui/trending/TrendingFragment.kt
@@ -1,4 +1,4 @@
-package com.lucas.historygreatests.ui.notifications
+package com.lucas.historygreatests.ui.trending
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.lucas.historygreatests.R
 
-class NotificationsFragment : Fragment() {
+class TrendingFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var viewModel: TrendingViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        viewModel =
+                ViewModelProviders.of(this).get(TrendingViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        viewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
