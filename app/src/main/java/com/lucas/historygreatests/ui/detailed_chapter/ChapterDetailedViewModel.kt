c514222f7503d6ba554814d0d59a711a2da50e0e
Lucas <lucasmontanari.97@gmail.com>
2020-10-02 23:28:46 -0300

    Add chapter detailed screen

---
+++ app/src/main/java/com/lucas/historygreatests/ui/detailed_chapter/ChapterDetailedViewModel.kt
@@ -0,0 +1,7 @@
+package com.lucas.historygreatests.ui.detailed_chapter
+
+import androidx.lifecycle.ViewModel
+
+class ChapterDetailedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
