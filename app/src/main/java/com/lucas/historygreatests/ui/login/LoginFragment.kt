8b1f0b7a4bdce949790ac325dec953df576942b3
Lucas <lucasmontanari.97@gmail.com>
2020-10-23 19:41:32 -0300

    Implement ViewBinding to Login

--- app/src/main/java/com/lucas/historygreatests/ui/login/LoginFragment.kt
+++ app/src/main/java/com/lucas/historygreatests/ui/login/LoginFragment.kt
@@ -2,21 +2,17 @@ package com.lucas.historygreatests.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.lucas.historygreatests.R
 import com.lucas.historygreatests.UserViewModel
-import kotlinx.android.synthetic.main.login_fragment.*
+import com.lucas.historygreatests.databinding.LoginFragmentBinding
 
 
 class LoginFragment : Fragment(R.layout.login_fragment) {
@@ -32,11 +28,13 @@ class LoginFragment : Fragment(R.layout.login_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val binding = LoginFragmentBinding.bind(view)
+
         savedStateHandle = findNavController().previousBackStackEntry!!.savedStateHandle
         savedStateHandle.set(LOGIN_SUCCESSFUL, false)
 
 
-        login_button.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             startSignIn()
         }
 
