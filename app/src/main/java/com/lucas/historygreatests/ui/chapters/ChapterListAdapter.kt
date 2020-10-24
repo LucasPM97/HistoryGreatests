cb280e7d197f15445b43d15a7780a2f80655c0c9
Lucas <lucasmontanari.97@gmail.com>
2020-10-23 22:27:30 -0300

    Clean RecyclerView Adapters

--- app/src/main/java/com/lucas/historygreatests/ui/chapters/ChapterListAdapter.kt
+++ app/src/main/java/com/lucas/historygreatests/ui/chapters/ChapterListAdapter.kt
@@ -2,11 +2,9 @@ package com.lucas.historygreatests.ui.chapters
 
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.lucas.historygreatests.R
 import com.lucas.historygreatests.databinding.FragmentChapterItemBinding
 import com.lucas.historygreatests.models.Chapter
 import com.lucas.historygreatests.utils.extensions.loadFromUrl
@@ -16,39 +14,12 @@ class ChapterListAdapter(
 ) : RecyclerView.Adapter<ChapterListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_chapter_item, parent, false)
-        return ViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = FragmentChapterItemBinding.inflate(inflater)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = chapters[position]
-
-        holder.binding.apply {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                rootView.clipToOutline = true
-            }
-            name.text = item.title
-            description.text = item.description
-            coloredBackground.setBackgroundColor(Color.parseColor(item.imageColor))
-            image.loadFromUrl(item.imageUrl.toString())
-            root.setOnClickListener {
-                val action =
-                    ChaptersFragmentDirections
-                        .actionNavigationChaptersToNavigationChaptersDetailed(
-                            item.chapter_id,
-                            item.imageUrl,
-                            item.title,
-                            item.imageColor,
-                            item.startYear,
-                            item.endYear
-                        )
-
-                it?.findNavController()?.navigate(action)
-            }
-        }
-
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(chapters[position])
 
     override fun getItemCount(): Int = chapters.size
 
@@ -57,7 +28,36 @@ class ChapterListAdapter(
         notifyDataSetChanged()
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val binding = FragmentChapterItemBinding.bind(view)
+    class ViewHolder(val binding:FragmentChapterItemBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(chapter:Chapter){
+            binding.apply {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    rootView.clipToOutline = true
+                }
+                name.text = chapter.title
+                description.text = chapter.description
+                coloredBackground.setBackgroundColor(Color.parseColor(chapter.imageColor))
+                image.loadFromUrl(chapter.imageUrl.toString())
+                root.setOnClickListener {
+                    onClick(chapter)
+                }
+            }
+        }
+
+        private fun onClick(chapter:Chapter){
+            val action = ChaptersFragmentDirections
+                    .actionNavigationChaptersToNavigationChaptersDetailed(
+                        chapter.chapter_id,
+                        chapter.imageUrl,
+                        chapter.title,
+                        chapter.imageColor,
+                        chapter.startYear,
+                        chapter.endYear
+
+                    );
+
+            binding.root.findNavController().navigate(action)
+        }
     }
 }
